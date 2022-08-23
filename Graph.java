7fa0f27bb7c01ba1b8570d4ab883db5f5efd0173
Rafael Teixeira <rafaelcteixeira1@hotmail.com>
2022-08-23 01:10:54 +0000

    Implementacao de busca em largura e connected

--- Graph.java
+++ Graph.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class Graph {
 
   private int countNodes;
@@ -93,4 +95,59 @@ public class Graph {
     }
     return newGraph;
   }
+
+  public float density() {
+    return (float) this.countEdges / (this.countNodes * (this.countNodes - 1));
+  }
+
+  public boolean subGraph(Graph g2) {
+    if (g2.countNodes > this.countNodes || g2.countEdges > this.countEdges)
+      return false;
+    for (int i = 0; i < g2.adjMatrix.length; ++i) {
+      for (int j = 0; j < g2.adjMatrix[i].length; ++j) {
+        if (g2.adjMatrix[i][j] != 0 && this.adjMatrix[i][j] == 0)
+          return false;
+      }
+    }
+    return true;
+  }
+
+  public ArrayList<Integer> buscaLargura(int s) {
+    int[] desc = new int[this.countNodes];
+    ArrayList<Integer> Q = new ArrayList<>();
+    ArrayList<Integer> R = new ArrayList<>();
+    desc[s] = 1;
+    Q.add(s);
+    R.add(s);
+
+    while (Q.size() > 0) {
+      int u = Q.remove(0);
+      for (int v = 0; v < this.adjMatrix[u].length; v++) {
+        if (this.adjMatrix[u][v] != 0) {
+          if (desc[v] == 0) {
+            Q.add(v);
+            R.add(v);
+            desc[v] = 1;
+          }
+        }
+      }
+    }
+    return R;
+  }
+
+  public void addUnorientedEdge(int u, int v, int w) {
+    if (u < 0 || u > this.countNodes - 1
+        || v < 0 || v > this.countNodes - 1 || w <= 0) {
+      System.err.println("Invalid input: " + u + v + w);
+      return;
+    }
+    this.adjMatrix[u][v] = w;
+    this.adjMatrix[v][u] = w;
+    this.countEdges += 2;
+  }
+
+  public boolean connected() {
+    return this.buscaLargura(7).size() == this.countNodes;
+  }
+
 }
--- Main.java
+++ Main.java
@@ -1,18 +1,32 @@
 class Main {
   public static void main(String[] args) {
-    Graph Graph = new Graph(4);
-    Graph.addEdge(0, 1, 3);
-    Graph.addEdge(0, 3, 4);
-    Graph.addEdge(1, 0, 3);
-    Graph.addEdge(3, 0, 4);
-    Graph.addEdge(3, 4, 2);
-    Graph.addEdge(2, 0, 2);
-    Graph.addEdge(2, 1, 2);
-    Graph.addEdge(2, 3, 2);
-    System.out.println(Graph);
-    System.out.println("Degree of a specific node: " + Graph.degree(3));
-    System.out.println("Highest degree: " + Graph.highestDegree());
-    System.out.println("Lowest degree: " + Graph.lowestDegree());
-    System.out.println(Graph.complement());
+    // Graph Graph = new Graph(4);
+    // Graph.addEdge(0, 1, 3);
+    // Graph.addEdge(0, 3, 4);
+    // Graph.addEdge(1, 0, 3);
+    // Graph.addEdge(3, 0, 4);
+    // Graph.addEdge(3, 4, 2);
+    // Graph.addEdge(2, 0, 2);
+    // Graph.addEdge(2, 1, 2);
+    // Graph.addEdge(2, 3, 2);
+    // System.out.println(Graph);
+    // System.out.println("Degree of a specific node: " + Graph.degree(3));
+    // System.out.println("Highest degree: " + Graph.highestDegree());
+    // System.out.println("Lowest degree: " + Graph.lowestDegree());
+    // System.out.println(Graph.complement());
+
+    Graph g1 = new Graph(10);
+    g1.addUnorientedEdge(7, 5, 1);
+    g1.addUnorientedEdge(7, 1, 1);
+    g1.addUnorientedEdge(7, 2, 1);
+    g1.addUnorientedEdge(1, 0, 1);
+    g1.addUnorientedEdge(1, 4, 1);
+    g1.addUnorientedEdge(2, 3, 1);
+    g1.addUnorientedEdge(5, 6, 1);
+    g1.addUnorientedEdge(6, 8, 1);
+
+    System.out.println(g1.buscaLargura(7));
+    System.out.println("Connectado: " + g1.connected());
+
   }
 }
